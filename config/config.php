a4b0d030bc88313d87de56d56cd647b1eca7d06d
galihsu <indosmartdg@gmail.com>
2023-09-18 14:51:05 +0700

    tambah kunci keamanan

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 // config.php
 
-define('API_KEY', 'Syaifull');
+define('API_KEY', '112a8f06-0fdc-4213-9af6-41a03e28186a');
--- index.php
+++ index.php
@@ -1,44 +1,50 @@
 <?php
+
+require_once('config/config.php'); // Mengimpor konstanta API_KEY
+require_once('functions/function.php');
+
+$headers = apache_request_headers();
+
 // Set header untuk menangani respons JSON
 header("Content-Type: application/json; charset=UTF-8");
 
 // Ambil URL dari permintaan
 $url = isset($_GET['url']) ? $_GET['url'] : '/';
 $method = $_SERVER['REQUEST_METHOD'];
-require_once('functions/function.php');
+$access = $headers['token'] === API_KEY ? true : false;
+if ($access) {
+	if ($url === 'getPaket') getPaket();
+	else if ($url === 'uploadFile') {
+		$img = $_FILES['img'];
+		if ($method === 'POST') upload($img);
+		else echo json_encode(array("message" => "Not Found"));
+	} else if ($url === 'permohonan') {
+		if ($method === 'POST') insertPermohonan();
+		else if ($method === 'GET') getPermohonan();
+		else echo json_encode(array("message" => "Not Found"));
+	} else if ($url === 'tokenFcm') {
+		if ($method === 'POST') postToken();
+		else if ($method === 'GET') getToken();
+		else echo json_encode(array("message" => "Not Found"));
+	} else if ($url === 'alamat') {
+		if ($method === 'POST') postAlamat();
+		else if ($method === 'GET') getAlamat();
+		else echo json_encode(array("message" => "Not Found"));
+	} else if ($url === 'notif') {
+		if ($method === 'POST') updatePesanTerbaca();
+		else if ($method === 'GET') getNotif();
+		else echo json_encode(array("message" => "Not Found"));
+	} else if ($url === 'pembayaran') {
+		if ($method === 'GET') getPembayaran();
+		else echo json_encode(array('message' => 'Not Found'));
+	} else if ($url === 'login') {
+		if ($method === 'POST') getLogin();
+		else echo json_encode(array('message' => 'Not Found'));
+	} else {
 
-if($url === 'getPaket') getPaket();
-else if($url === 'uploadFile'){
-	$img = $_FILES['img'];
-	if($method === 'POST')upload($img);
-	else echo json_encode(array("message" => "Not Found"));
-}else if($url === 'permohonan'){
-	if($method === 'POST') insertPermohonan();
-	else if($method === 'GET') getPermohonan();
-	else echo json_encode(array("message" => "Not Found"));
-}else if($url === 'tokenFcm'){
-	if($method === 'POST') postToken();
-	else if($method === 'GET') getToken();
-	else echo json_encode(array("message" => "Not Found"));
-}else if($url === 'alamat'){
-	if($method === 'POST') postAlamat();
-	else if ($method === 'GET') getAlamat();
-	else echo json_encode(array("message" => "Not Found"));
-}else if($url === 'notif'){
-	if($method === 'POST') updatePesanTerbaca();
-	else if ($method === 'GET') getNotif();
-	else echo json_encode(array("message" => "Not Found"));
-}else if($url === 'pembayaran'){
-	 if($method === 'GET') getPembayaran();
-	 else echo json_encode(array('message' => 'Not Found'));
-}else if($url === 'login'){
-	if($method === 'POST') getLogin();
-	else echo json_encode(array('message' => 'Not Found'));
-
+		http_response_code(404);
+		echo json_encode(array("message" => "Not Found"));
+	}
+} else {
+	echo json_encode(array('message' => 'Permission Denied'));
 }
-else {
-
-    http_response_code(404);
-    echo json_encode(array("message" => "Not Found"));
-}
- 
