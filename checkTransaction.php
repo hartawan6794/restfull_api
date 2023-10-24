65fb1b534b5254ffa829c55abf5586c425911a6a
galihsu <indosmartdg@gmail.com>
2023-10-24 13:11:18 +0700

    penambahan config duitku

---
+++ checkTransaction.php
@@ -0,0 +1,51 @@
+<?php
+
+    $json = file_get_contents('php://input');
+    $result = json_decode($json);
+    
+    // Set your merchant code (Note: Server key for sandbox and production mode are different)
+    $merchantCode = "DS17093"; 
+    // Set your merchant key (Note: Server key for sandbox and production mode are different)
+    $merchantKey = "1e69910d4338feaa6fcd80a86d21581c";
+    
+    $reference = $result->{'reference'}; 
+    $signature = md5($merchantCode . $reference . $merchantKey);
+
+    $itemsParam = array(
+        'merchantCode' => $merchantCode,
+        'signature' => $signature
+    );
+    
+    class emp{}
+    
+    $params = array_merge((array)$result,$itemsParam);
+    $params_string = json_encode($params);
+    
+    //if sandbox
+    $url = 'http://sandbox.duitku.com/webapi/api/merchant/transactionStatus';
+    //if production
+    //$url = 'https://passport.duitku.com/webapi/api/merchant/transactionStatus';
+    
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);                                                                  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json',                                                                                
+        'Content-Length: ' . strlen($params_string))                                                                       
+    );   
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    //execute post
+    $request = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($httpCode == 200)
+    {
+	      echo $request;
+    }
+    else
+        echo $httpCode;
+?>
